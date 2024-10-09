e69f66d39ea17938c99b7d77e73f4ac62e6fd078
Azugot <augustoscardua@gmail.com>
2024-10-09 17:33:43 -0300

    Re-organized the damm buttons

--- Main.py
+++ Main.py
@@ -22,39 +22,38 @@ class CropApp:
         self.app = root
         self.app.title('CROP')
         self.app.geometry('700x700')
-        self.title = Label(self.app, text='CROP THE IMAGE', font='arial 30 bold', fg='#068481')
-        self.title.pack()
 
         self.image_area = Canvas(self.app, width=uiWidth, height=uiHeight, bg='#C8C8C8')
-        self.image_area.pack(pady=(10,0))
 
         self.open_image = Button(self.app, width=20, text='OPEN IMAGE', font='none 12', command=self.openAndPut)
-        self.open_image.pack(pady=(10,5))
         
         self.open_mat = Button(self.app, width=20, text='OPEN MAT DATASET', font='none 12', command=self.read_mat_files)
-        self.open_mat.pack(pady=(10,5))
         
         self.next_pacient = Button(self.app, width=20, text='NEXT PACIENT', font='none 12', command=self.next_mat_pacient)
-        self.next_pacient.pack(pady=(10,5))
         
         self.previous_pacient = Button(self.app, width=20, text='PREVIOUS PACIENT', font='none 12', command=self.previous_mat_pacient)
-        self.previous_pacient.pack(pady=(10,5))
 
         self.next_pacient_image = Button(self.app, width=20, text='NEXT PACIENT IMAGE', font='none 12', command=self.next_mat_pacient_image)
-        self.next_pacient_image.pack(pady=(10,5))
         
         self.previous_pacient_image = Button(self.app, width=20, text='PREVIOUS PACIENT IMAGE', font='none 12', command=self.previous_mat_pacient_image)
-        self.previous_pacient_image.pack(pady=(10,5))
-
-        self.crop_area = Button(self.app, width=20, text='SELECT AREA', font='none 12', command=self.select_area)
-        self.crop_area.pack(pady=(0,5))
 
         self.show_area = Button(self.app, width=20, text='SHOW AREA', font='none 12', command=self.show_mask)
-        self.show_area.pack(pady=(0,5))
 
         self.save_image = Button(self.app, width=20, text='SAVE IMAGE', font='none 12', command=self.save_image)
-        self.save_image.pack()
 
+        self.image_area.grid(row=0, column=0)
+        self.show_area.grid(row=1, column=0)
+        
+        self.previous_pacient_image.grid(row=0, column=1)
+        self.next_pacient_image.grid(row=0, column=2)
+        
+        self.previous_pacient.grid(row=1, column=1)
+        self.next_pacient.grid(row=1, column=2)
+        
+        self.open_mat.grid(row=3, column=1)
+        self.open_image.grid(row=4, column=1)
+        self.save_image.grid()
+        
     def openAndPut(self):
         self.mat_file_is_open = False
         self.mat_file = None
@@ -67,12 +66,7 @@ class CropApp:
             self.image = ImageTk.PhotoImage(self.image)
             self.image_area.create_image(0, 0, image=self.image, anchor='nw')
     
-    def get_x_and_y(self, event):
-        self.lasx, self.lasy = event.x, event.y
-
-    def draw_smth(self, event):
-        self.image_area.create_line((self.lasx, self.lasy, event.x, event.y), fill='red', width=3)
-        self.lasx, self.lasy = event.x, event.y
+    
 
         if 0 <= self.lasx < 500 and 0 <= self.lasy < 400:
             self.mask[self.lasy][self.lasx] = 0 
@@ -80,10 +74,6 @@ class CropApp:
             self.mask[self.lasy-1][self.lasx-1] = 0 
             self.mask[self.lasy+1][self.lasx-1] = 0 
             self.mask[self.lasy-1][self.lasx+1] = 0 
-
-    def select_area(self):
-        self.image_area.bind("<Button-1>", self.get_x_and_y)
-        self.image_area.bind("<B1-Motion>", self.draw_smth)
 
     def retrun_shape(self, image_in):
         image = image_in
